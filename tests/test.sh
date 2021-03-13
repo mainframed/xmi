4cd4ced4c267720a30b46decb52ad8f0c771d807
mainframed <mainframed767@gmail.com>
2021-03-13 07:00:19 -0800

    fixing tests

--- tests/test.sh
+++ tests/test.sh
@@ -4,10 +4,10 @@
 # in this folder.
 
 echo "Testing XMI files"
-for i in *.xmi; do echo "python3 ../xmi.py $i"; python3 ../xmi.py $i; done
+for i in *.xmi; do echo "python3 ../xmi/__init__.py $i";python3 ../xmi/__init__.py $i; done
 
 echo "Testing AWS files"
-for i in *.aws; do echo "python3 ../xmi.py $i";python3 ../xmi.py $i; done
+for i in *.aws; do echo "python3 ../xmi/__init__.py $i";python3 ../xmi/__init__.py $i; done
 
 echo "Testing HET files"
-for i in *.het; do echo "python3 ../xmi.py $i";python3 ../xmi.py $i; done
+for i in *.het; do echo "python3 ../xmi/__init__.py $i";python3 ../xmi/__init__.py $i; done
