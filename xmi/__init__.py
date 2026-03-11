8a7f5186a60973c9ab873e4e3cd14e1686ca1be1
Phil <mainframed767@gmail.com>
2026-03-11 11:21:43 -0700

    updating the version number for pypi

--- xmi/__init__.py
+++ xmi/__init__.py
@@ -46,7 +46,7 @@ xmi Python Library
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-__version__ = '0.6.0'
+__version__ = '1.0.0'
 __author__ = 'Philip Young, Henri Kuiper'
 __license__ = "GPL"
 
