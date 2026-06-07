0da8669af4f963001cd8a37800c4b1565e26547c
Wizard of z/OS <wizard@zdevops.com>
2026-06-07 10:47:45 +0200

    Bump 1.0.4

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Philip Young'
 author = 'Philip Young, Henri Kuiper'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.3'
+release = '1.0.4'
 
 
 # -- General configuration ---------------------------------------------------
